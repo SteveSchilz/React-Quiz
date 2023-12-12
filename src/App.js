e59f645c14ef44412f5a3a4e88dcdeabebfbd82f
Steve Schilz <steve.schilz@gmail.com>
2023-12-12 15:21:58 -0800

    Read Data from jsonServer into a useReducer state variable
    * Define the useReducer state and dispatch functions
    * define the reducer function with switch statement for loading, ready and error cases
    * dispatch an action when data read successfully
    * handle error if fetch fails - e.g. if failed to launch json server with npm run server

--- src/App.js
+++ src/App.js
@@ -1,14 +1,48 @@
-import { useReducer } from "react";
+import { useEffect, useReducer } from "react";
 import Header from "./Header.js";
 import Main from "./Main.js";
+import Loader from "./Loader.js";
+
+// STATUSES: loading, error, ready, active, finished
+
+const initialState = {
+  questions: [],
+  status: "loading",
+};
+
+// ACTIONS:
+// * dataReceived: fetch data completed.
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return { ...state, status: "ready", questions: action.payload };
+    case "dataFailed":
+      return { ...state, status: "error", questions: [] };
+    case "":
+      return { ...state };
+    default:
+      throw new Error("Unknown action type: " + action.type);
+  }
+}
 
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(function () {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
+  }, []);
+
   return (
     <div className="app">
       <Header />
       <Main>
         <p>1/15</p>
-        <p>Question</p>
+        {state.status === "loading" && <Loader />}
+        {state.status === "ready" && <p>Ready</p>}
+        {state.status === "error" && <p>Error!</p>}
       </Main>
     </div>
   );
